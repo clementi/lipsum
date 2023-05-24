c8870ab9b0404da04a47f91dbe86ffecd140cac1
Jeffrey Pratt <clementi@users.noreply.github.com>
2023-05-23 19:12:24 -0600

    Remove default for --amount

--- src/Opts.hs
+++ src/Opts.hs
@@ -38,6 +38,6 @@ versionOption = infoOption "0.0.0" (long "version" <> short 'V' <> help "Show ve
 programOptions :: Parser Opts
 programOptions =
   Opts
-    <$> option auto (long "unit" <> short 'u' <> metavar "UNIT" <> value Paragraphs <> help "Set the text unit (paras, words, or bytes)")
-    <*> option auto (long "amount" <> short 'a' <> metavar "AMOUNT" <> value 2 <> help "Set the amount of text units")
+    <$> option auto (long "unit" <> short 'u' <> metavar "UNIT" <> value Paragraphs <> help "Set the text unit (paras (default), words, or bytes)")
+    <*> option auto (long "amount" <> short 'a' <> metavar "AMOUNT" <> help "Set the amount of text units")
     <*> switch (long "start" <> short 's' <> help "Do not start with 'Lorem ipsum'")
